d9b02cbf022249995ba933c44fb46b9f6817d4a7
zsegel <zachary.segel@gmail.com>
2017-09-28 20:43:38 -0400

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,4 +1,9 @@
 # use regret matching to compute a best-response strategy to an opponent's RPS strategy
 
-def RPS_response()
-    pass
+def RPS_response(opp, sims=10000):
+    """Compute best response strategy to a given RPS strategy."""
+    
+    games_played = 1
+    cumulative_regrets = [0, 0, 0]
+    my_strategy = [1/3., 1/3., 1/3.]
+    
